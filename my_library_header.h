4a5c3289a8d06882e79ac2fe3be891d00d003655
mariamkiran <mariam.kiran@gmail.com>
2014-12-28 15:31:02 +0000

    Create my_library_header.h

---
+++ my_library_header.h
@@ -0,0 +1,7 @@
+#define MAXIMUM_N_GENERATED 5
+#define TRANSACTION_FEE 0.25 //can be in bitcoins rather than generating new bitcoins
+//#define choices 1 :sell / 0: buying
+#define LENGTH 10
+#define PROB_INTRO 0.05
+#define AVER 500
+
